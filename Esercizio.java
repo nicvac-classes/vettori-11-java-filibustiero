04a01fabfebbcda0315f9b050c13c2eaaf50bc3a
ivn <giovanni.signorile@marconibari.edu.it>
2024-12-03 22:10:41 +0100

    Update Esercizio.java

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,56 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+Ecco il codice in Java: 
 
-//Import di Classi Java necessarie al funzionamento del programma
+```java
 import java.util.Scanner;
+import java.util.Random;
 
-// Classe principale, con metodo main
-class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+        System.out.print("Inserisci il numero di cifre (N): ");
+        int N = scanner.nextInt();
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        int[] U = new int[N];
+        int[] R = new int[N];
+        int[] S = new int[N];
+        double[] M = new double[N];
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        for (int i = 0; i < N; i++) {
+            int num;
+            do {
+                System.out.print("Inserisci un numero intero positivo per U[" + i + "]: ");
+                num = scanner.nextInt();
+            } while (num < 0);
+            U[i] = num;
+        }
+
+        for (int i = 0; i < N; i++) {
+            R[i] = random.nextInt(11);
+            S[i] = random.nextInt(6) + 3;
+            M[i] = (U[i] + R[i] + S[i]) / 3.0;
+        }
+
+        System.out.println("Vettore M:");
+        for (double val : M) {
+            System.out.printf("%.2f ", val);
+        }
+        System.out.println();
+
+        System.out.print("Inserisci un numero intero T: ");
+        int T = scanner.nextInt();
+
+        System.out.println("Coppie del vettore U con somma uguale a T:");
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (U[i] + U[j] == T) {
+                    System.out.println("(" + U[i] + ", " + U[j] + ")");
+                }
+            }
+        }
+
+        scanner.close();
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+```
